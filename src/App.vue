b0eddeaefbc12a5ca1fbf2885725895517f18bb2
Casto1387 <c.castorina@student.maastrichtuniversity.nl>
2021-01-08 14:13:25 +0100

    changed project priority

--- src/App.vue
+++ src/App.vue
@@ -53,31 +53,35 @@ export default {
       projects: [
         {
           title: "Claudio Castorina's Portfolio",
-          desc: "That's right, that's the page you're looking at now! At the moment I think it's the site that most reflects my style, it was developed with Vue.js 3 and nothing else! All CSS, HTML and Javascript was developed by hand to ensure the most personalized experience possible.",
+          desc:
+            "That's right, that's the page you're looking at now! At the moment I think it's the site that most reflects my style, it was developed with Vue.js 3 and nothing else! All CSS, HTML and Javascript was developed by hand to ensure the most personalized experience possible.",
           img: "portfolio.png",
           git: "https://github.com/Casto1387/portfolio",
           target: "_blank",
         },
         {
+          title: "Camera D'Ascolto",
+          desc:
+            '"Camera D\'Ascolto" was definitely the most difficult site to develop, as I was specifically required to use Mobirise, a "what you see is what you get" type editor. The hard part was to edit all the blocks to the lowest possible level and to be able to make it all work. The heroic act is on Github, although getting the hang of it is very complicated.',
+          img: "cameradascolto.jpg",
+          git: "https://github.com/Casto1387/cameradascolto",
+          target: "_blank",
+        },
+        {
           title: "Cimenailfilm",
-          desc: "Cimenailfilm was the first site I created, and unfortunately I no longer have the source code. The site can be found on the wayback machine, and in any case I wanted to mention it because it was the beginning of everything, for me. It was entirely made in Bootstrap.",
+          desc:
+            "Cimenailfilm was the first site I created, and unfortunately I no longer have the source code. The site can be found on the wayback machine, and in any case I wanted to mention it because it was the beginning of everything, for me. It was entirely made in Bootstrap.",
           img: "cimenailfilm.jpeg",
           git: "#",
           isDisabled: true,
         },
         {
           title: "Firewall Gaming",
-          desc: "Firewall Gaming was one of the most famous eSports associations in Italy around the 2015s. The site was created on Laravel, and my job was to develop the front-end side. The website is not availably on github for copyright reasons, but I'm available to show it to anyone interested.",
+          desc:
+            "Firewall Gaming was one of the most famous eSports associations in Italy around the 2015s. The site was created on Laravel, and my job was to develop the front-end side. The website is not availably on github for copyright reasons, but I'm available to show it to anyone interested.",
           img: "firewallgaming.jpg",
           git: "#",
           isDisabled: true,
-        },
-        {
-          title: "Camera D'Ascolto",
-          desc: '"Camera D\'Ascolto" was definitely the most difficult site to develop, as I was specifically required to use Mobirise, a "what you see is what you get" type editor. The hard part was to edit all the blocks to the lowest possible level and to be able to make it all work. The heroic act is on Github, although getting the hang of it is very complicated.',
-          img: "cameradascolto.jpg",
-          git: "https://github.com/Casto1387/cameradascolto",
-          target: "_blank",
         },
       ],
       isCurious: false,
